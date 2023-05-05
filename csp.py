a4ebbba3f56ad8efe9bf81abb420031d801afc21
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-05-05 10:14:38 +0330

    created the vanilla backtrack

--- csp.py
+++ csp.py
@@ -1,41 +1,112 @@
-class Constraint:
-    def __init__(self, variables, type):
-        self.variables = variables
-        self.type = type
-
-    def is_satisfied(self, assignment):
-        # Check if the constraint is satisfied by the current assignment of values to variables
-        if self.type == "row":
-            values = [assignment[var] for var in self.variables]
-            return len(set(values)) == len(values) and all(value != 0 for value in values)
-        elif self.type == "column":
-            values = [assignment[var] for var in self.variables]
-            return len(set(values)) == len(values) and all(value != 0 for value in values)
-        elif self.type == "subgrid":
-            values = [assignment[var] for var in self.variables]
-            return len(set(values)) == len(values) and all(value != 0 for value in values)
-        else:
-            return True
-
+from typing import List
 
 class Variable:
-    def __init__(self, row, col, domain):
+    def __init__(self, row: int, col: int, domain: List[int]):
         self.row = row
         self.col = col
         self.domain = domain
-        self.value = None
+        self.value =  None if len(domain) > 1 else domain[0]
 
-    def assign(self, value):
-        self.value = value
+    def __str__(self):
+        return f'({self.row}, {self.col}): {self.value}'
 
-    def unassign(self):
-        self.value = None
 
-    def is_assigned(self):
-        return self.value is not None
-    
-    def get_possible_values(self):
-        if self.is_assigned():
-            return [self.value]
-        else:
-            return self.domain
+class Constraint:
+    def __init__(self, variables: List[Variable]):
+        self.variables = variables
+
+    def is_satisfied(self, assignment: List[Variable]) -> bool:
+        pass
+
+    def __str__(self):
+        return f'{[var for var in self.variables]}'
+
+
+class SudokuConstraint(Constraint):
+    def __init__(self, variables: List[Variable]):
+        super().__init__(variables)
+
+    def is_satisfied(self, assignment: List[Variable]) -> bool:
+        values = [var.value for var in assignment if var in self.variables]
+        #print(values)
+        return len(values) == len(set(values))
+
+
+class CSP:
+    def __init__(self, variables: List[Variable], constraints: List[Constraint]):
+        self.variables = variables
+        self.constraints = constraints
+
+    def get_related_variables(self, var: Variable) -> List[Variable]:
+        related_vars = []
+        for constraint in self.constraints:
+            if var in constraint.variables:
+                for related_var in constraint.variables:
+                    if related_var != var and related_var not in related_vars:
+                        related_vars.append(related_var)
+        return related_vars
+
+    def is_consistent(self, var: Variable, value: int, assignment: List[Variable]) -> bool:
+        var.value = value
+        assignment.append(var)
+        for constraint in self.constraints:
+            if not constraint.is_satisfied(assignment):
+                var.value = None
+                assignment.pop()
+                return False
+        var.value = None
+        assignment.pop()
+        return True
+
+    def assign(self, var: Variable, value: int):
+        var.value = value
+
+    def unassign(self, var: Variable):
+        var.value = None
+
+def mrv(csp: CSP, assignment: List[Variable]) -> Variable:
+    unassigned_vars = [var for var in csp.variables if var.value is None]
+    sorted_vars = sorted(unassigned_vars, key=lambda var: len(var.domain))
+    return sorted_vars[0]
+
+def ac3(csp: CSP, queue: List[tuple]) -> bool:
+    while queue:
+        (Xi, Xj) = queue.pop(0)
+        if revise(csp, Xi, Xj):
+            if len(Xi.domain) == 0:
+                return False
+            for Xk in csp.get_related_variables(Xi):
+                if Xk != Xj:
+                    queue.append((Xk, Xi))
+    return True
+
+def revise(csp: CSP, Xi: Variable, Xj: Variable) -> bool:
+    revised = False
+    for value in Xi.domain:
+        if not csp.is_consistent(Xi, value, [csp.assign(Xj, value)]):
+            Xi.domain.remove(value)
+            revised = True
+    return revised
+
+def order_domain_values(var: Variable, assignment: List[Variable], csp: CSP) -> List[int]:
+    return var.domain
+
+def backtrack(csp: CSP, assignment: List[Variable]=[]) -> List[Variable]:
+    if all(var.value is not None for var in csp.variables):
+        return assignment
+    var = mrv(csp, assignment)
+    for value in order_domain_values(var, assignment, csp):
+        if csp.is_consistent(var, value, assignment):
+            csp.assign(var, value)
+            assignment.append(var)
+            # queue = [(related_var, var) for related_var in csp.get_related_variables(var)]
+            # if ac3(csp, queue):
+            #     result = backtrack(csp, assignment)
+            #     if result is not None:
+            #         return result
+            result = backtrack(csp, assignment)
+            if result is not None:
+                return result
+            csp.unassign(var)
+            assignment.pop()
+    return None
--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,45 @@
-from csp import Constraint
+from csp import *
 
-class SudokuBoard:
-    def __init__(self, board):
-        self.board = board
-        self.variables = [(i, j) for i in range(9) for j in range(9)]
-        self.domains = {(i, j): set(range(1, 10)) if board[i][j] == 0 else set([board[i][j]]) for i in range(9) for j in range(9)}
-        self.constraints = self.create_constraints()
+def solve_sudoku_csp(board: list[list[int]]) -> list[list[int]]:
+    variables = []
+    constraints = []
+    assignment = []
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                domain = list(range(1, 10))
+            else:
+                domain = [board[i][j]]
+            variable = Variable(i, j, domain)
+            if board[i][j] != 0:
+                assignment.append(variable)
+            variables.append(variable)
 
-    def create_constraints(self):
-        # Create a list of constraints based on the Sudoku rules
-        constraints = []
-        for i in range(9):
-            row_vars = [(i, j) for j in range(9)]
-            col_vars = [(j, i) for j in range(9)]
-            subgrid_vars = [(i//3*3+j//3, i%3*3+j%3) for j in range(9)]
-            constraints += [Constraint(row_vars, "row"), Constraint(col_vars, "column"), Constraint(subgrid_vars, "subgrid")]
-        return constraints
+    for i in range(9):
+        row_vars = [var for var in variables if var.row == i]
+        row_constraint = SudokuConstraint(row_vars)
+        #print(*row_vars)
+        constraints.append(row_constraint)
+
+    for j in range(9):
+        col_vars = [var for var in variables if var.col == j]
+        col_constraint = SudokuConstraint(col_vars)
+        #print(*col_vars)
+        constraints.append(col_constraint)
+
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            block_vars = [var for var in variables if var.row in range(i, i+3) and var.col in range(j, j+3)]
+            block_constraint = SudokuConstraint(block_vars)
+            #print(*block_vars)
+            constraints.append(block_constraint)
+    
+    csp = CSP(variables, constraints)
+    result = backtrack(csp, assignment)
+    if result is not None:
+        for var in result:
+            board[var.row][var.col] = var.value
+    # for const in constraints:
+    #     print(*const.variables)
+
+    return board
