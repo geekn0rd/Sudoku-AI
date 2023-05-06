c949ecdfb28a1e2cc3cb4c54dfc7a42318750f1e
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-05-06 14:49:05 +0330

    generalized the board to nxn

--- sudoku.py
+++ sudoku.py
@@ -1,35 +1,36 @@
 from csp import CSP
 
-def sudoku_csp(grid):
+def sudoku_csp(grid, n=9):
     variables = []
     domains = {}
-    for i in range(9):
-        for j in range(9):
+    root_n = int(n**0.5)
+    for i in range(n):
+        for j in range(n):
             if grid[i][j] == 0:
                 variables.append((i, j))
-                domains[(i, j)] = set(range(1, 10))
+                domains[(i, j)] = set(range(1, n + 1))
             else:
                 domains[(i, j)] = set([grid[i][j]])
     neighbors = {}
-    for i in range(9):
-        for j in range(9):
-            row = [(i, y) for y in range(9) if y != j]
-            col = [(x, j) for x in range(9) if x != i]
-            box = [(x, y) for x in range((i//3)*3, (i//3)*3+3) 
-                          for y in range((j//3)*3, (j//3)*3+3) if x != i or y != j]
+    for i in range(n):
+        for j in range(n):
+            row = [(i, y) for y in range(n) if y != j]
+            col = [(x, j) for x in range(n) if x != i]
+            box = [(x, y) for x in range((i//root_n)*root_n, (i//root_n)*root_n+root_n) 
+                          for y in range((j//root_n)*root_n, (j//root_n)*root_n+root_n) if x != i or y != j]
             neighbors[(i, j)] = set(row + col + box)
     constraints = lambda xi, x, xj, y: x != y
     return CSP(variables, domains, neighbors, constraints)
 
-def solve_sudoku(grid):
-    csp = sudoku_csp(grid)
+def solve_sudoku(grid, n=9):
+    csp = sudoku_csp(grid, n)
     csp.ac3()
     solution = csp.backtracking_search()
     if solution is None:
         print("Unsolvable CSP!")
     else:
-        for i in range(9):
-            for j in range(9):
+        for i in range(n):
+            for j in range(n):
                 if (i, j) in solution:
                     grid[i][j] = solution[(i, j)]
         return grid
