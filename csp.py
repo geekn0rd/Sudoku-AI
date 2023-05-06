774ecc1711a380bc2ace98711d6df4a06c922617
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-05-06 16:49:54 +0330

    added LCV heuristic

--- csp.py
+++ csp.py
@@ -1,5 +1,7 @@
+from typing import Callable
+
 class CSP:
-    def __init__(self, variables: list, domains: dict, neighbors: dict, constraints: function):
+    def __init__(self, variables: list, domains: dict, neighbors: dict, constraints: Callable):
         self.variables = variables
         self.domains = domains
         self.neighbors = neighbors
@@ -30,7 +32,7 @@ class CSP:
         if len(assignment) == len(self.variables):
             return assignment
         var = self.select_unassigned_variable(assignment)
-        for value in self.order_domain_values(var):
+        for value in self.order_domain_values(var, assignment):
             if self.is_consistent(var, value, assignment):
                 assignment[var] = value
                 result = self.backtracking_search(assignment)
@@ -43,8 +45,17 @@ class CSP:
         unassigned = [v for v in self.variables if v not in assignment]
         return self.min_remaining_values(unassigned)
     
-    def order_domain_values(self, var: tuple) -> list:
-        return self.domains[var]
+    def order_domain_values(self, var: tuple, assignment: dict) -> list:
+        domain_values = []
+        for value in self.domains[var]:
+            count = 0
+            for neighbor in self.neighbors[var]:
+                if neighbor not in assignment:
+                    if value in self.domains[neighbor]:
+                        count += 1
+            domain_values.append((value, count))
+        domain_values.sort(key=lambda x: x[1])
+        return [x[0] for x in domain_values]
     
     def is_consistent(self, var: tuple, value: int, assignment: dict) -> bool:
         for neighbor in self.neighbors[var]:
